868e20e97d3b1498bfecf613a1d85b4abe15103e
Tsutomu Nagura <nagura@d6.dion.ne.jp>
2024-05-01 12:55:28 +0900

    Added Azure current models

--- types/openai.ts
+++ types/openai.ts
@@ -8,38 +8,61 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_4 = 'gpt-4',
-  GPT_4_32K = 'gpt-4-32k',
+  //GPT_3_5 = 'gpt-3.5-turbo',
+  //GPT_3_5_AZ = 'gpt-35-turbo',
+  //GPT_4 = 'gpt-4',
+  //GPT_4_32K = 'gpt-4-32k',
+  GPT_3_5_AZ_16K = 'gpt-35-turbo-16k',
+  GPT_4_AZ = 'gpt-4',
+  GPT_4_AZ_32K = 'gpt-4-32k',
+
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_3_5_AZ_16K;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
+  //[OpenAIModelID.GPT_3_5]: {
+  //  id: OpenAIModelID.GPT_3_5,
+  //  name: 'GPT-3.5',
+  //  maxLength: 12000,
+  //  tokenLimit: 4000,
+  //},
+  //[OpenAIModelID.GPT_3_5_AZ]: {
+  //  id: OpenAIModelID.GPT_3_5_AZ,
+  //  name: 'GPT-3.5',
+  //  maxLength: 12000,
+  //  tokenLimit: 4000,
+  //},
+  //[OpenAIModelID.GPT_4]: {
+  //  id: OpenAIModelID.GPT_4,
+  //  name: 'GPT-4',
+  //  maxLength: 24000,
+  //  tokenLimit: 8000,
+  //},
+  //[OpenAIModelID.GPT_4_32K]: {
+  //  id: OpenAIModelID.GPT_4_32K,
+  //  name: 'GPT-4-32K',
+  //  maxLength: 96000,
+  //  tokenLimit: 32000,
+  //},
+  [OpenAIModelID.GPT_3_5_AZ_16K]: {
+    id: OpenAIModelID.GPT_3_5_AZ_16K,
+    name: 'GPT-3.5-16k',
+    maxLength: 48000,
+    tokenLimit: 16384,
   },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-  },
-  [OpenAIModelID.GPT_4]: {
-    id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
+  [OpenAIModelID.GPT_4_AZ]: {
+    id: OpenAIModelID.GPT_4_AZ,
+    name: 'GPT-4-8k',
     maxLength: 24000,
     tokenLimit: 8000,
   },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
+  [OpenAIModelID.GPT_4_AZ_32K]: {
+    id: OpenAIModelID.GPT_4_AZ_32K,
+    name: 'GPT-4-32k',
     maxLength: 96000,
     tokenLimit: 32000,
   },
+
 };
